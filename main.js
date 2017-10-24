a1929caa1bf3e9f2605bd0256012d698e901a86d
Alex Wildman <Alex>
2017-10-24 16:48:21 -0600

    found hover time length

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 // total time spent on page
 
 $(document).ready(function() {
+	var start;
+	var end;
+
+
+
+
 	// start = Date.getTime();
 	// var now = new Date ()
 	var start = Date.now();
@@ -21,15 +27,29 @@ var totalLength = documentHeight - windowHeight;
 var percentageScrolled = Math.floor(scrollTop/totalLength * 100)
 console.log(percentageScrolled + '% viewed')
 
+//time spent hovering over each element
+$('.pug-image1').mouseover(function() {
+	start  = Date.now();
+});
+
+$('.pug-image1').mouseleave(function() {
+	end = Date.now();
+	console.log((end - start) / 100);
+});
+
+
+// how many vowels typed in input
+var vowels = function() {
+	if($('.input').val() )
+	var sss = $('.input').val()
+	if()
+console.log(sss);
+
+};
 
 
 
-// $('body').click(function(){
-// })
 
-// $('input').click(function(event){
-// 	console.log(event);
-// })
 // var finalReport = $(window).on('beforeunload', function(event){
 // 	return 
 //          Needs to return
@@ -38,7 +58,6 @@ console.log(percentageScrolled + '% viewed')
 //         3) time spent hovering over each element on the page
 //         4) how many vowels they typed in the input
 //         5) if they left page by clicking a link, record which link
-//         5)
 
 // 		time-spent on page - take date method from page load and subtract from leaving page and do the math
 //         scroll-top - find what percentage of the page they viewed
