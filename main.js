359ccae0c12735e99cc256b65ec689a59bf67f4e
Alex Wildman <Alex>
2017-10-25 08:55:20 -0600

    fixed vowel counter

--- main.js
+++ main.js
@@ -3,13 +3,17 @@
 $(document).ready(function() {
 	var start;
 	var end;
-	var vowel;
+	var vowel = 0;
+	var hovering;
+	var timespent;
+	var amountviewed;
+	var vowelstyped
 
 	// time spent on page
 	start = Date.now();
 	$(window).on('beforeunload',function(event) {
 		end = Date.now();
-		console.log("User spent " + (end - start) / 1000 + "seconds");
+		timespent = ("User spent " + (end - start) / 1000 + " seconds on the page");
 	});
 
 	// percentage the user viewed
@@ -18,7 +22,7 @@ $(document).ready(function() {
 	var scrollTop = $(window).scrollTop();
 	var totalLength = documentHeight - windowHeight;
 	var percentageScrolled = Math.floor(scrollTop/totalLength * 100)
-	console.log(percentageScrolled + '% viewed')
+	amountviewed = (percentageScrolled + '% viewed')
 
 	//time spent hovering over each element
 	//first pug image
@@ -27,41 +31,90 @@ $(document).ready(function() {
 	});
 	$('.pug-image1').mouseleave(function() {
 		end = Date.now();
-		console.log((end - start) / 100);
+		hovering += ("User spent " + ((end - start) / 100) + " seconds on the first pug image");
 	});
 	//first paragraph
-
+	$('.paragraph1').mouseover(function() {
+	start  = Date.now();
+	});
+	$('.paragraph1').mouseleave(function() {
+		end = Date.now();
+		hovering += ("User spent " + ((end - start) / 100) + " seconds on the first paragraph");
+	});
 	//second pug picture
-
+	$('.pug-image2').mouseover(function() {
+	start  = Date.now();
+	});
+	$('.pug-image2').mouseleave(function() {
+		end = Date.now();
+		hovering += ("User spent " + ((end - start) / 100) + " seconds on the second pug image");
+	});
 	//first link
-
-
+	$('.link1').mouseover(function() {
+	start  = Date.now();
+	});
+	$('.link1').mouseleave(function() {
+		end = Date.now();
+		hovering += ("User spent " + ((end - start) / 100) + " seconds on the first link");
+	});
 	//second paragraph
-
+	$('.paragraph2').mouseover(function() {
+	start  = Date.now();
+	});
+	$('.paragraph2').mouseleave(function() {
+		end = Date.now();
+		hovering += ("User spent " + ((end - start) / 100) + " seconds on the second paragraph");
+	});
 
 	//second link
-
+	$('.link2').mouseover(function() {
+	start  = Date.now();
+	});
+	$('.link2').mouseleave(function() {
+		end = Date.now();
+		hovering += ("User spent " + ((end - start) / 100) + " seconds on the second link");
+	});
 	//third pug picture
-
+	$('.pug-image3').mouseover(function() {
+	start  = Date.now();
+	});
+	$('.pug-image3').mouseleave(function() {
+		end = Date.now();
+		hovering += ("User spent " + ((end - start) / 100) + " seconds on the third pug image");
+	});
 
 	//input field
+	$('.input').mouseover(function() {
+	start  = Date.now();
+	});
+	$('.input').mouseleave(function() {
+		end = Date.now();
+		hovering += ("User spent " + ((end - start) / 100) + " seconds on the input field");
+	});
 	// how many vowels typed in input
-	$('.input').keyup(function(){
-		var x = $('.input').val();
-		if (x === 'a' || x === 'e' || x === 'i' || x === 'o' || x === 'u') {
-			vowel = x;
-			console.log(vowel)
-	}
+	$(document).keydown(function(event){
+		
+		// var x = $('.input').val();
+			if (event.key === 'a' || event.key === 'e' || event.key === 'i' || event.key === 'o' || event.key === 'u') {
+				vowel += 1;
+				vowelstyped = ("User typed " + vowel + " vowels");
+			}
 	});
 
 	// which link did they click
 	$('link').click(function(){
 		console.log($('link').val());
+	});
+
+	var finalReport = $(window).on('beforeunload', function(event){
+		console.log(timespent);
+		console.log(amountviewed);
+		console.log(vowelstyped);
+		console.log();
+		console.log(hovering);
 	})
 });
 
-// var finalReport = $(window).on('beforeunload', function(event){
-// 	return 
 //          Needs to return
 //         1) total time spent on page
 //         2) what percentage of the page they viewed ( how far they scrolled down)
@@ -71,7 +124,3 @@ $(document).ready(function() {
 
 // 		time-spent on page - take date method from page load and subtract from leaving page and do the math
 //         scroll-top - find what percentage of the page they viewed
-
-
-        
-//     })
